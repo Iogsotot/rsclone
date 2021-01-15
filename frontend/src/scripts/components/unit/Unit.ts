00e89a2cebb61a283f43d20e9e40a7cc592d16d1
Iogsotot <ajustusa@gmail.com>
2021-01-15 16:00:33 +0300

    fix: anims for Unit.ts

--- frontend/src/scripts/components/unit/Unit.ts
+++ frontend/src/scripts/components/unit/Unit.ts
@@ -32,9 +32,9 @@ export default class Unit extends Phaser.GameObjects.PathFollower {
     
     // this.create(texture);
     // console.log(this);
-    this.play({ key: `${unitType}_walk`, repeat:  Infinity});
+    this.play({ key: `${this.unitType}_walk`, repeat:  Infinity});
     this.setInteractive();
-    this.on("pointerdown", this.takeDamage, this)
+    this.on("pointerdown", this.onEnemyClicked, this)
     // hitArea
     // texture
   }
@@ -43,23 +43,34 @@ export default class Unit extends Phaser.GameObjects.PathFollower {
     console.log(`${this.unitType}_walk`);
   }
 
+  onEnemyClicked() {
+    if(this.alive === false) {
+      return false;
+    }
+    else if(this.alive === true) {
+      this.takeDamage();
+    } 
+  }
+
   takeDamage() {
     if(this.hp - 15 <= 15) {
       this.hp = 0;
       this.die();
     } else if(this.hp >= 15) {
       this.hp -= 15;
-      this.play({ key: `${this.unitType}_hurt`, repeat: 0});
-      this.on('animationcomplete', () => {this.play({ key: `${this.unitType}_walk`, repeat:  Infinity})}, this)
+      console.log(this.anims.getName());
+
+      if (this.anims.getName() === `${this.unitType}_walk`) {
+        this.play(`${this.unitType}_hurt`);
+        this.chain([{ key: `${this.unitType}_walk`, repeat:  Infinity}]);
+      }
     }
-    console.log(this.hp)
   }
 
   die() {
     if (this.alive) {
       this.alive = false;
       this.pauseFollow();
-      //может как-нибудь запретить другие анимации при die() ?
       this.play({ key: `${this.unitType}_die`, repeat: 0});
       this.on('animationcomplete', this.despawn, this)
     }
