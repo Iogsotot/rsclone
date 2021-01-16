91704155516fdb935f8c6d9086b819f2c95ed6d0
Iogsotot <ajustusa@gmail.com>
2021-01-16 21:01:19 +0300

    fix: App.ts

--- frontend/src/scripts/App.ts
+++ frontend/src/scripts/App.ts
@@ -3,13 +3,11 @@ import Phaser from 'phaser';
 import runAuth from './auth/run.auth';
 import config from './components/Game';
 import createBgGame from './auth/utils/create.bg';
-import generateLevelsMap from './levels-map/generateLevelsMap';
 
 window.addEventListener('load', () => {
   runAuth();
   document.querySelector('.logo-start-button')?.addEventListener('click', () => {
-    // createBgGame();
-    generateLevelsMap()
-    // const game = new Phaser.Game(config);
+    createBgGame();
+    const game = new Phaser.Game(config);
   });
 });
