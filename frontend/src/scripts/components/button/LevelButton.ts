377e5f70c1646fb46a61afa5a37f3486aa6df44b
Iogsotot <ajustusa@gmail.com>
2021-01-16 21:03:21 +0300

    feat: add LevelButton

---
+++ frontend/src/scripts/components/button/LevelButton.ts
@@ -0,0 +1,26 @@
+import Button from './Button';
+import { levelsConfig } from '../../constants/constants';
+import StartScreenModal from '../modal/StartScreenModal';
+
+export default class LevelButton extends Button {
+  level: number;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, btnTexture: string, level: number) {
+    super(scene, x, y, btnTexture);
+    this.level = level;
+  }
+
+  handleDown() {
+    const modal = new StartScreenModal(
+      this.scene,
+      levelsConfig[`level_${this.level}`].map.scaleCoordinateTowers.length,
+      'modal-bg',
+      'title-bg',
+      this.level
+    );
+
+    modal.startNewBtn.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, () => {
+      this.scene.scene.start('game-scene', {level: this.level, difficulty: 1});
+    });
+  }
+}
