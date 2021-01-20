d4731ef28cc4f457ea2edbd22391f78225721372
Iogsotot <ajustusa@gmail.com>
2021-01-20 21:58:21 +0300

    refactor: return old version of PlayerStats.ts

--- frontend/src/scripts/components/stats/PlayerStats.ts
+++ frontend/src/scripts/components/stats/PlayerStats.ts
@@ -1,15 +1,65 @@
-import { getCurrentPlayerStats, setCurrentPlayerStats } from '../../backend'
+type AchievStats = {
+  firstAsterisk: boolean,
+  completeVictory: boolean,
+  firstBlood: boolean,
+  GreatDefender: boolean,
+  IronDefender: boolean,
+  killer: boolean,
+  seller: boolean,
+  builder: boolean
+};
 
-async function getPlayerStatsFromServer(userId): Promise<object> {
-  const token = localStorage.getItem('token');
-  const response = await getCurrentPlayerStats({ id: userId, token });
-  return response
+export default class PlayerStats {
+  userId: string;
+
+  gameProgress: number;
+
+  gameLogInCount: number;
+
+  killedEnemies: number;
+
+  builtTowers: number;
+
+  soldTowers: number;
+
+  ironModeProgress: number;
+
+  achievements: AchievStats;
+  constructor({
+    userId,
+    gameProgress,
+    gameLogInCount,
+    killedEnemies,
+    builtTowers,
+    soldTowers,
+    ironModeProgress,
+    achievements
+  }) {
+    this.userId = userId,
+    this.gameProgress = gameProgress;
+    this.gameLogInCount = gameLogInCount;
+    this.killedEnemies = killedEnemies;
+    this.builtTowers = builtTowers;
+    this.soldTowers = soldTowers;
+    this.ironModeProgress = ironModeProgress;
+    this.achievements = achievements;
+  }
 }
 
-async function sendPlayerStatsToServer(userId, data): Promise<object> {
-  const token = localStorage.getItem('token');
-  const response = await setCurrentPlayerStats({ id: userId, token, body: data });
-  return response
-}
+// import { getCurrentPlayerStats, setCurrentPlayerStats } from '../../backend'
 
-export {getPlayerStatsFromServer, sendPlayerStatsToServer};
+// async function getPlayerStatsFromServer(userId): Promise<object> {
+//   const token = localStorage.getItem('token');
+//   const response = await getCurrentPlayerStats({ id: userId, token });
+//   return response
+// }
+
+// async function sendPlayerStatsToServer(userId, data): Promise<object> {
+//   const token = localStorage.getItem('token');
+//   const response = await setCurrentPlayerStats({ id: userId, token, body: data });
+//   return response
+// }
+
+// export {getPlayerStatsFromServer, sendPlayerStatsToServer};
+
+
