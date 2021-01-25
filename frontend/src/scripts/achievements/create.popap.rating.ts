4945b9771a42aaa741e74c21c6b8cb06a040417d
mrinex <mrinex@mail.ru>
2021-01-25 23:54:10 +0300

    docs: add title

--- frontend/src/scripts/achievements/create.popap.rating.ts
+++ frontend/src/scripts/achievements/create.popap.rating.ts
@@ -2,12 +2,21 @@ import createElement from '../auth/utils/createElement';
 import whileLoad from '../auth/utils/wait.while.loading';
 
 function popapRatingCreate(stats) {
+  console.log('rating data:', stats);
+
   const popup = createElement('div', {
     classList: ['popup-rating-wrapper'],
     innerHTML: `
       <div class="popup-rating-content">
         <div class="title-rating">Overall rating</div>
         <div class="close-rating-popup"></div>
+        <div class="wrapper-table-rating">
+          <div class="title-rating-property">
+            <div>Player's name</div>
+            <div>game progress</div>
+            <div>achievements</div>
+          </div>
+        </div>
       </div>
     `,
     onclick: ({ target }) => {
