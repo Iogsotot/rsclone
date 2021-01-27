80cba555783d118b176ebe474f0fd8a685c7016b
mrinex <mrinex@mail.ru>
2021-01-27 20:41:23 +0300

    feat: try send to server

--- frontend/src/scripts/achievements/utils/backend.ts
+++ frontend/src/scripts/achievements/utils/backend.ts
@@ -1,24 +1,34 @@
 import { KEY_ID, KEY_TOKEN, LOCAL_STORAGE_KEY } from '../../constants/constants';
 import { getCurrentPlayerStats, setCurrentPlayerStats } from '../../backend';
-// import { PlayerStatsManager } from "../components/stats/PlayerStats";
-// const playerStats = new PlayerStatsManager();
 
-async function sendDataToBackend(data) {
-  // const id = localStorage.getItem(KEY_ID);
-  // const token = localStorage.getItem(KEY_TOKEN);
-  // const currentStateFromServer = await getCurrentPlayerStats({ id, token });
+async function sendDataToBackend() {
+  const id = localStorage.getItem(KEY_ID);
+  const token = localStorage.getItem(KEY_TOKEN);
+  const currentStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "");
+  const { data } = await getCurrentPlayerStats({ id, token });
+  const {
+    userId, login, builtTowers, soldTowers,
+    killedEnemies, achievements, gameProgress, ironModeProgress
+  } = data;
+  console.log('currentStorage:', currentStorage);
+  console.log('currentStateFromServer: ', data);
 
-  // const isUpdate = await setCurrentPlayerStats({
-  //   id,
-  //   token,
-  //   body: {
-  //     ...currentStateFromServer,
-  //     builtTowers: currentStateFromServer.builtTowers + builtTowers,
-  //     soldTowers: currentStateFromServer.soldTowers + soldTowers,
-  //     killedEnemies: currentStateFromServer.killedEnemies + killedEnemies,
-  //   },
-  // });
-  // console.log('isUpdate level:',isUpdate);
+  const isUpdate = await setCurrentPlayerStats({
+    id,
+    token,
+    body: {
+      ...data,
+      userId,
+      login,
+      builtTowers: builtTowers + currentStorage.builtTowers,
+      soldTowers: soldTowers + currentStorage.soldTowers,
+      killedEnemies: killedEnemies + currentStorage.killedEnemies,
+      achievements: currentStorage.achievements,
+      gameProgress: currentStorage.gameProgress,
+      ironModeProgress: currentStorage.ironModeProgress,
+    },
+  });
+  console.log('sendDataToBackend [result]:', isUpdate);
 }
 
 export default sendDataToBackend;
