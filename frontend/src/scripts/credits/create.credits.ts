77117c28740ca0bd4b79aaa606a0ca44cec2d921
mrinex <mrinex@mail.ru>
2021-01-28 21:49:04 +0300

    feat: add gratitude and our team style and data

--- frontend/src/scripts/credits/create.credits.ts
+++ frontend/src/scripts/credits/create.credits.ts
@@ -9,21 +9,39 @@ function createCredits() {
         <div class="title-credits">Credits</div>
         <div class="close-credits-popup"></div>
         <div class="credits-data">
-          <div class="credits-people">
-            <div class="avatar Iogsotot"></div>
-            <a href="https://github.com/Iogsotot">Iogsotot</a>
+          <h2 class="credits-title">Our team</h2>
+          <div class="wrapper-credits-people">
+            <div class="credits-people">
+              <div class="avatar Iogsotot"></div>
+              <a href="https://github.com/Iogsotot">Iogsotot</a>
+            </div>
+            <div class="credits-people">
+              <div class="avatar DenisAfa"></div>
+              <a href="https://github.com/DenisAfa">DenisAfa</a>
+            </div>
+            <div class="credits-people">
+              <div class="avatar Abdulloh76"></div>
+              <a href="https://github.com/Abdulloh76">Abdulloh76</a>
+            </div>
+            <div class="credits-people">
+              <div class="avatar mrINEX"></div>
+              <a href="https://github.com/mrINEX">mrINEX</a>
+            </div>
           </div>
-          <div class="credits-people">
-            <div class="avatar DenisAfa"></div>
-            <a href="https://github.com/DenisAfa">DenisAfa</a>
-          </div>
-          <div class="credits-people">
-            <div class="avatar Abdulloh76"></div>
-            <a href="https://github.com/Abdulloh76">Abdulloh76</a>
-          </div>
-          <div class="credits-people">
-            <div class="avatar mrINEX"></div>
-            <a href="https://github.com/mrINEX">mrINEX</a>
+          <h2 class="credits-title">Gratitude</h2>
+          <div class="wrapper-gratitude">
+            <div class="gratitude-from">
+              <div>Gratitude from people</div>
+              <p>s lekrnflwa;vjrtvanlei jrtblq i3jn5ljntilqejr j 34qimgjcl g  3qjig4 cm3g jg  j4ij m ij3 4lijmgml  h34gl</p>
+            </div>
+            <div class="gratitude-from">
+              <div>Gratitude from people</div>
+              <p>s lekrnflwa;vjrtvanlei jrtblq i3jn5ljntijig4 cm3g jg  j4ij m ij3 4lijmgml  h34gl</p>
+            </div>
+            <div class="gratitude-from">
+              <div>Gratitude from people</div>
+              <p>s lekrnflwa;vjrtvanlei jrtblq i3jn5ljntilqejr j 3vjrtvanlei jrtblq i3jn5ljntijig4 cm3g jg  j4ij m vjrtvanlei jrtblq i3jn5ljntijig4 cm3g jg  j4ij m vjrtvanlei jrtblq i3jn5ljntijig4 cm3g jg  j4ij m 4qimgjcl g  3qjig4 cm3g jg  j4ij m ij3 4lijmgml  h34gl</p>
+            </div>
           </div>
         </div>
       </div>
